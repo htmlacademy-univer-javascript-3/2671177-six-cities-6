2a6f84dec8f34d2f4ee581b18e5725247b7eb056
llizz.kh <lizakhan2001@mail.ru>
2025-10-08 23:04:46 +0300

    add app component and offersCount prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const offersCount = 5;
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App cardsCount={offersCount} />
   </React.StrictMode>
 );
